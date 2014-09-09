e050aeb95f24f1733b258621e3417f8ddf9989d7
Qiming Chen <qc2195@columbia.edu>
2014-09-09 00:47:00 -0400

    first commit
    shell can identify "exit" now

--- shell/shell.c
+++ shell/shell.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_INPUT_LENGTH 1<<16
+
 int main(int argc, char **argv) {
+	while (1){
+		printf("$");
+		char inputString[MAX_INPUT_LENGTH];
+		scanf("%s", inputString);
+		/*
+			exit case
+		*/
+		if (!strcmp(inputString, "exit")){
+			break;
+		}
+	}
 	return 0;
 }
