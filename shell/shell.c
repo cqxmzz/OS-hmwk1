02fdc0127da1efab0cf6ddc24af172821203d18a
Qiming Chen <qc2195@columbia.edu>
2014-09-09 02:28:59 -0400

    support any length of line GNU coding style

--- shell/shell.c
+++ shell/shell.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define MAX_INPUT_LENGTH 1<<16
+char *read_line()
+{
+        char *input_string = NULL;
+        int chunk = 4;
+        char temp_buffer[chunk];
+        int input_length = 0;
+        int temp_length = 0;
+        do {
+                fgets(temp_buffer, chunk, stdin);
+                temp_length = strlen(temp_buffer);
+                input_length += temp_length;
+                if (input_string == NULL) {
+                        input_string = malloc((input_length + 1) * sizeof(char));
+                        input_string[0] = '\0';
+                }
+                else{
+                        input_string = realloc(input_string, input_length + 1);
+                }
+                strcat(input_string, temp_buffer);
+        } while (temp_length == chunk - 1 && temp_buffer[chunk - 2] != '\n');
+        input_string[input_length - 1] = '\0';
+        return input_string;
+}
 
-int main(int argc, char **argv) {
-	while (1){
-		printf("$");
-		char inputString[MAX_INPUT_LENGTH];
-		scanf("%s", inputString);
-		/*
-			exit case
-		*/
-		if (!strcmp(inputString, "exit")){
-			break;
-		}
-	}
-	return 0;
+int main(int argc, char **argv) 
+{
+        while (1){
+                printf("$");
+                char * input_string = NULL;
+                input_string = read_line();
+                /*
+                 * exit case
+                 */
+                if (!strcmp(input_string, "exit")){
+                        break;
+                }
+                free(input_string);
+                input_string = NULL;
+        }
+        return 0;
 }
