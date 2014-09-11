61f6b475214da5069cd50f940fdfd1b98dd5cd36
Qiming Chen <qc2195@columbia.edu>
2014-09-11 01:34:44 -0400

    path add delete OK, need path to work with exec

--- shell/shell.c
+++ shell/shell.c
@@ -4,8 +4,14 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #define MAX_NUMBER_OF_ARGS 500
+
+int number_of_paths = 0;
+char **paths = NULL;
+int path_capacity = 0;
 
 char *read_line()
 {
@@ -46,7 +52,7 @@ int parse(char *input_string, int max_number_of_args, char *args[], int *number_
         int i;
         for (i = 0; i < strlen(input_string) + 1; ++i){
                 if (input_string[i] == ' ' || i == strlen(input_string)){
-                        char *arg = (char *)malloc(i - former_space);
+                        char *arg = (char *)malloc((i - former_space) * sizeof(char));
                         if (arg == NULL){
                                 fprintf(stderr, "error: %s\n", "Cannot require space to store the argument, perhaps too long?");
                                 return -1;
@@ -66,10 +72,131 @@ int parse(char *input_string, int max_number_of_args, char *args[], int *number_
         return 0;
 }
 
+int cd(char * path)
+{
+        if (path == NULL)
+                return -1;
+        if (chdir(path) == 0)
+                return 0;
+        else{
+                fprintf(stderr, "error: %s\n", strerror(errno));                                 
+                return -1;
+        }
+}
+
+int show_paths()
+{
+        int i;
+        for (i = 0; i < number_of_paths - 1; ++i)
+                printf("%s:", paths[i]);
+        if (number_of_paths > 0)
+                printf("%s", paths[number_of_paths - 1]);
+        return 0;
+}
+
+int add_path(char * path)
+{
+        if (path == NULL){
+                fprintf(stderr, "error: %s\n", "Path not exist");
+                return -1;
+        }
+        if (path == NULL){
+                fprintf(stderr, "error: %s\n", "Bad path");
+                return -1;
+        }
+        int i;
+        for (i = 0; i < number_of_paths; ++i){
+                if (!strcmp(path, paths[i])){
+                        fprintf(stderr, "error: %s\n", "Path existed");
+                        return -1;
+                }
+        }
+        if (paths == NULL){
+                paths = (char **)malloc(sizeof(char *));
+                path_capacity = 1;
+                if (paths == NULL){
+                        fprintf(stderr, "error: %s\n", "Cannot require space to store the path, perhaps too many paths?");
+                        return -1;
+                }
+        }
+        if (number_of_paths != 0 && number_of_paths == path_capacity){
+                char **temp_paths = (char **)malloc(2 * number_of_paths * sizeof (char *));
+                if (paths == NULL){
+                        fprintf(stderr, "error: %s\n", "Cannot require space to store the path, perhaps too many paths?");
+                        return -1;
+                }
+                int i;
+                for (i = 0; i < number_of_paths; ++i)
+                        temp_paths[i] = paths[i];
+                for (i = number_of_paths; i < 2 * number_of_paths; ++i)
+                        temp_paths[i] = NULL;
+                free(paths);
+                paths = temp_paths;
+                path_capacity *= 2;
+        }
+        paths[number_of_paths] = (char *)malloc(sizeof(char) * strlen(path));
+        if (paths[number_of_paths] == NULL){
+                fprintf(stderr, "error: %s\n", "Cannot require space to store the path, perhaps too many paths?");
+                return -1;
+        }
+        strcpy(paths[number_of_paths], path);
+        number_of_paths++;
+        return 0;
+}
+
+int delete_path(char * path)
+{
+        if (path == NULL){
+                fprintf(stderr, "error: %s\n", "Bad path");
+                return -1;
+        }
+        int path_deleted = 0;
+        int i;
+        for (i = 0; i < number_of_paths; ++i)
+        {
+                if (!strcmp(path, paths[i]))
+                {
+                        int j;
+                        char *temp = paths[i];
+                        for (j = i; j < number_of_paths - 1; ++j){
+                                paths[j] = paths[j+1];
+                        }
+                        number_of_paths--;
+                        free(temp);
+                        temp = NULL;
+                        path_deleted = 1;
+                        i--;
+                }
+        }
+        if (path_deleted == 0){
+                fprintf(stderr, "error: %s\n", "Path not found");
+                return -1;
+        }
+        return 0;
+}
+
+int execute(char **args)
+{
+        int pid = fork();
+        if (pid == 0){
+                if (execv(args[0], args, "/bin/") == -1)
+                {
+                        fprintf(stderr, "error: %s\n", strerror(errno));
+                        return -1;
+                }
+                else
+                        return 0;
+        }else if (pid > 0){
+                wait(0);
+                return 0;
+        }else{
+                fprintf(stderr, "error: %s\n", strerror(errno));
+                return -1; 
+        }
+}
+
 int main(int argc, char **argv) 
 {
-        int number_of_paths = 0;
-        char **paths = NULL;
         while (1){
                 printf("$");
                 char *input_string = NULL;
@@ -99,55 +226,36 @@ int main(int argc, char **argv)
                  * cd
                  */
                 if (!strcmp(args[0], "cd")){
-                        if (chdir(args[1]) == 0)
-                                continue;
-                        else{
-                               fprintf(stderr, "error: %s\n", strerror(errno));                                 
-                               continue;
-                       }
+                        cd(args[1]);
                 }
                 /*
                  * paths
                  */
                 if (!strcmp(args[0], "path")){
                         if (args[1] == NULL){
-                                int i;
-                                for (i = 0; i < number_of_paths; ++i)
-                                        printf("%s\n", paths[i]);
-                        }
-                        if (!strcmp(args[1], "+"))
+                                show_paths();
+                        }else if (!strcmp(args[1], "+"))
                         {
-
+                                add_path(args[2]);
+                        }else if (!strcmp(args[1], "-")){
+                                delete_path(args[2]);
+                        }else{
+                                fprintf(stderr, "error: %s\n", "Path command error");
                         }
-                        if (!strcmp(args[1], "-"))
+                        continue;
                 }
                 /*
                  * execute
                  */
-                int pid = fork();
-                if (pid == 0){
-                        if (execv(args[0], args) == -1)
-                        {
-                                fprintf(stderr, "error: %s\n", strerror(errno));
-                                exit(EXIT_FAILURE);
-                        }
-                        else
-                                exit(EXIT_SUCCESS);
-                }else if (pid > 0){
-                        wait(0);
-                }else{
-                        fprintf(stderr, "error: %s\n", strerror(errno));
-                        continue; 
-                }
-                int i;
-                for (i = 0; i < number_of_args; ++i){
-                            free(args[i]);
-                            args[i] = NULL;
-                }
+                execute(args);
                 /*
                  * free memory
                  */
-                free(args);
+                int i;
+                for (i = 0; i < number_of_args; ++i){
+                        free(args[i]);
+                        args[i] = NULL;
+                }
                 free(input_string);
                 input_string = NULL;
         }
